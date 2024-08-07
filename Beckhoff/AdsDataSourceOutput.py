6e9eaf50200ce7a20fdbc194e6eb42faef4eca62
Jun Jiang <jun.jiang@eonerc.rwth-aachen.de>
2024-08-07 16:49:29 +0200

    More implementation of Beckhoff Signed-off-by: Jun Jiang <jun.jiang@eonerc.rwth-aachen.de>

--- Beckhoff/AdsDataSourceOutput.py
+++ Beckhoff/AdsDataSourceOutput.py
@@ -4,19 +4,19 @@ Module AdsDataSourceOutput: Interface of ADS (Beckhoff TwinCAT) to DataLogger
 What is Automation Device Specification (ADS):
     See https://infosys.beckhoff.com/english.php?content=../content/1033/tcinfosys3/11291871243.html&id=
 """
-from Base.DataSource import DataSourceBase
-from Base.DataOutput import DataOutputBase
-from Base import Auxiliary
-from typing import TypedDict
+
+from Base import DataSource, DataOutput
 import pyads
+import time
 import os
+import sys
 import logging.config
 # Load logging configuration from file
 logging.config.fileConfig(r'_config/logging.ini')
 logger = logging.getLogger('ADS')
 
 
-class AdsDataSourceOutput(DataSourceBase, DataOutputBase):
+class AdsDataSourceOutput:
     # Class attribute: ADS states
     _ads_states = {
         pyads.ADSSTATE_INVALID: 'Invalid',  # 0
@@ -74,59 +74,113 @@ class AdsDataSourceOutput(DataSourceBase, DataOutputBase):
         30: 'Access denied – secure ADS access denied',
     }
 
+    class AdsDataSource(DataSource.DataSourceBase):
+        """Inner class for data source"""
+        def __init__(self, plc: pyads.Connection, all_data_names: list[str]):
+            super().__init__()
+            self.plc = plc  # Instance of plc in main class
+            self._all_data_names = all_data_names
+
+        def read_data(self) -> list:
+            return list(self.plc.read_list_by_name(self._all_data_names).values())
+
+    class AdsDataOutput(DataOutput.DataOutputBase):
+        """Inner class for data output"""
+        def __init__(self, plc: pyads.Connection, all_data_names: list[str]):
+            super().__init__(all_data_names=all_data_names, time_in_header=False)  # Without time header
+            self.plc = plc  # Instance of plc in main class
+
+        def _log_data(self, row: list):
+            """Log data"""
+            def clean_keys_with_none_values(input_dict: dict) -> dict:
+                """Clean keys that have none values"""
+                keys_of_none_values = [k for k, v in input_dict.items() if v is None]
+                if len(keys_of_none_values) > 0:
+                    logger.debug(f"Found keys with none values: '{keys_of_none_values}, removing these keys ...")
+                    for _k in keys_of_none_values:
+                        del input_dict[_k]
+                return input_dict
+
+            write_dict = dict(zip(self._headers, row))  # Zip headers with data
+            write_dict_cleaned = clean_keys_with_none_values(write_dict)  # Clean none values
+            if len(write_dict_cleaned) > 0:
+                self.plc.write_list_by_name(write_dict_cleaned)
+            else:
+                logger.warning("No more keys after cleaning the write dict, skipping logging ...")
+
     def __init__(
             self,
             ams_net_id: str,
             ams_net_port: int = pyads.PORT_TC3PLC1,
-            read_data_names: list[str] | None = None,
-            write_data_names: list[str] | None = None
+            source_data_names: list[str] | None = None,
+            output_data_names: list[str] | None = None
     ):
         """TODO: Describe add router"""
         logger.info("Initializing AdsDataSourceOutput ...")
         self.ams_net_id = ams_net_id
         self.ams_net_port = ams_net_port
-        self.read_data_names = read_data_names
-        self.write_data_names = write_data_names
+        self._source_data_names = source_data_names
+        self._output_data_names = output_data_names
 
-        # Init state
-        self.plc: pyads.Connection
+        # Config PLC
+        self.plc = pyads.Connection(self.ams_net_id, self.ams_net_port)
+
+        # Init connection state
         self.plc_connected = False
+
+        # Init inner classes
+        # Data source
+        if self._source_data_names is None:
+            self._data_source = None
+        else:
+            logger.info("Initializing AdsDataSource ...")
+            self._data_source = self.AdsDataSource(self.plc, self._source_data_names)
+        # Data output
+        if self._output_data_names is None:
+            self._data_output = None
+        else:
+            logger.info("Initializing AdsDataOutput ...")
+            self._data_output = self.AdsDataOutput(self.plc, self._output_data_names)
 
         # Connect PLC with retries
         self._plc_connect_with_retry(max_retries=5, retry_period=2)
+        if self.plc_connected:
+            logger.info(f"Connect to PLC successfully")
+        else:
+            logger.error(f"Connect to PLC failed, exiting ...")
+            sys.exit(1)
 
     def _plc_connect(self):
         """Try to connect PLC only once"""
         if self.plc_connected:
             # Read PLC state
-            _plc_state = self._plc_read_state()
-            logger.info(f"PLC already connected: ADS state - '{_plc_state[0]}', device state - '{_plc_state[1]}'")
+            plc_state = self._plc_read_state()
+            logger.info(f"PLC already connected: ADS state - '{plc_state[0]}', device state - '{plc_state[1]}'")
         else:
             try:
                 logger.info(f"Connecting PLC ...")
                 # Connect PLC
-                self.plc = pyads.Connection(self.ams_net_id, self.ams_net_port)
                 self.plc.open()
 
                 # Read PLC state
-                _plc_state = self._plc_read_state()
-                logger.info(f"PLC connected: ADS state - '{_plc_state[0]}', device state - '{_plc_state[1]}'")
+                plc_state = self._plc_read_state()
+                logger.info(f"PLC connected: ADS state - '{plc_state[0]}', device state - '{plc_state[1]}'")
 
                 # Update state
                 self.plc_connected = True
             except pyads.ADSError:
-                logger.error(f"PLC connection failed")
+                logger.warning(f"PLC connection failed")
 
     def _plc_connect_with_retry(self, max_retries: int = 5, retry_period: int = 2):
-        """Connect PLC with retries"""
-        _attempt = 1
-        while _attempt <= max_retries:
-            logger.info(f"Connecting PLC with retries {_attempt}/{max_retries} ...")
+        """Connect PLC with multiple retries"""
+        attempt = 1
+        while attempt <= max_retries:
+            logger.info(f"Connecting PLC with attempt(s): {attempt}/{max_retries} ...")
             self._plc_connect()
-            if self.plc_connected or _attempt == max_retries:
+            if self.plc_connected or attempt == max_retries:
                 break
             else:
-                _attempt += 1
+                attempt += 1
                 time.sleep(retry_period)
 
     def _plc_close(self):
@@ -140,48 +194,61 @@ class AdsDataSourceOutput(DataSourceBase, DataOutputBase):
         _ads_state_int, _device_state_int = self.plc.read_state()
         return self._ads_states.get(_ads_state_int), self._ads_return_codes.get(_device_state_int)
 
-    def get_all_read_data_names(self, from_plc: bool = True) -> list[str]:
-        """Get all read data names"""
-        if from_plc:
-            return list(self.plc.read_list_by_name(self.read_data_names).keys())
-        else:
-            return self.read_data_names
-
     def read_data(self) -> list | None:
-        if self.plc_connected:
-            if self.read_data_names is None:
-                logger.error(f"No data names are defined for reading")
-                return None
+        if isinstance(self._data_source, self.AdsDataSource):
+            if self.plc_connected:
+                return self._data_source.read_data()
             else:
-                return list(self.plc.read_list_by_name(self.read_data_names).values())
+                logger.warning(f"PLC disconnected, reconnecting ...")
+                self._plc_connect()
         else:
-            self._plc_connect()  # Try to connect PLC once
-            return None
+            raise AttributeError("No data source exists, please check the initialization")
 
     def log_data(self, row: list):
-        if self.plc_connected:
-            if self.write_data_names is None:
-                logger.error(f"No data names are defined for writing")
+        if isinstance(self._data_output, self.AdsDataOutput):
+            if self.plc_connected:
+                return self._data_output.check_and_log_data(row)
             else:
-                _write_dict = dict(zip(self.write_data_names, row))
-                self.plc.write_list_by_name(_write_dict)
+                logger.warning(f"PLC disconnected, reconnecting ...")
+                self._plc_connect()
         else:
-            self._plc_connect()  # Try to connect PLC once
+            raise AttributeError("No data output exists, please check the initialization")
+
+    @property
+    def data_source(self):
+        return self._data_source
+
+    @property
+    def data_output(self):
+        return self._data_output
+
+    @property
+    def ads_states(self):
+        return self._ads_states
+
+    @property
+    def ads_return_codes(self):
+        return self._ads_return_codes
 
 
 if __name__ == '__main__':
+    from Base import Auxiliary, DataLogger
+
     # Init ADS
     ads_source_output = AdsDataSourceOutput(
         ams_net_id='5.78.127.222.1.1',
-        read_data_names=Auxiliary.load_json('_config/AdsReadDataExamples.json')[:20],
-        write_data_names=Auxiliary.load_json('_config/AdsWriteDataExamples.json')
+        source_data_names=Auxiliary.load_json('_config/AdsReadDataExamples.json')[:20],
+        output_data_names=Auxiliary.load_json('_config/AdsWriteDataExamples.json')
     )
 
     # Init csv output
-    csv_output = DataLogger.DataOutputCsv(file_name=os.path.join('Test', 'csv_logger.csv'), all_data_names=)
+    csv_output = DataLogger.DataOutputCsv(
+        file_name=os.path.join('Test', 'csv_logger.csv'),
+        all_data_names=ads_source_output.data_source.all_data_names
+    )
 
     # Init random source
-    random_source = DataLogger.RandomDataSource()
+    random_source = DataSource.RandomDataSource(size=2, missing_rate=0.5)
 
     # Init DataLoggers
     test_logger_read = DataLogger.DataLoggerBase(
