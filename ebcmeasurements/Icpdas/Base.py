52388132a39d0188031501aac748b362aab83da2
Jun Jiang <jun.jiang@eonerc.rwth-aachen.de>
2024-10-16 14:10:24 +0200

    Finish I-87K series Signed-off-by: Jun Jiang <jun.jiang@eonerc.rwth-aachen.de>

--- ebcmeasurements/Icpdas/Base.py
+++ ebcmeasurements/Icpdas/Base.py
@@ -3,7 +3,8 @@ Base module for ICP DAS
 
 Introduction of I/O modules: https://www.icpdas.com/root/product/solutions/remote_io/rs-485/i-8k_i-87k/i-8k_i-87k_introduction.html
 """
-from abc import ABC, abstractmethod
+from abc import ABC
+import re
 import socket
 import sys
 import logging.config
@@ -12,6 +13,19 @@ logger = logging.getLogger(__name__)
 
 
 class EthernetIoBase(ABC):
+    """Base class for I/O units and modules"""
+    # Class attribute: baud rate settings
+    _baud_rate_settings = {
+        '03': 1200,
+        '04': 2400,
+        '05': 4800,
+        '06': 9600,
+        '07': 19200,
+        '08': 38400,
+        '09': 57600,
+        '0A': 115200,
+    }
+
     @staticmethod
     def _to_hex(value: int) -> str:
         """Converts an integer (dec) to hex with 2 digits"""
@@ -24,7 +38,7 @@ class EthernetIoBase(ABC):
     def decode_response(
             response: str,
             parse: dict[str, tuple[int, int]],
-    ):
+    ) -> dict[str, str | float | int] | None:
         """
         Decode the response by parse
         :param response: Response in utf-8 format
@@ -37,6 +51,8 @@ class EthernetIoBase(ABC):
             for key, (index_start, index_stop) in par.items():
                 if key == 'address_id':
                     decoded_response[key] = int(rsp[index_start: index_stop + 1], 16)
+                elif key == 'baud_rate_code':
+                    decoded_response['baud_rate'] = EthernetIoBase._baud_rate_settings.get(rsp[index_start: index_stop + 1])
                 else:
                     decoded_response[key] = rsp[index_start: index_stop + 1]
             return decoded_response
@@ -45,7 +61,7 @@ class EthernetIoBase(ABC):
             # No response due to time out
             logger.info(f"No data received due to time out or error")
             return None
-        elif response.startswith(('!', '>', '<')):  # TODO: check the delimiters
+        elif response.startswith(('!', '>')):
             # Valid response
             return _decode_general(rsp=response, par=parse)
         elif response.startswith('?'):
@@ -68,7 +84,7 @@ class EthernetIoUnit(EthernetIoBase, ABC):
         # Establish socket connection
         self._establish_socket_connection()
 
-    def _establish_socket_connection(self):
+    def _establish_socket_connection(self) -> None:
         """Establish socket connection to I/O"""
         logger.info(f"Establishing socket connection to {self.host}:{self.port} ...")
         if self.socket is None:
@@ -84,25 +100,63 @@ class EthernetIoUnit(EthernetIoBase, ABC):
         else:
             logger.info(f"Socket connection already established: {self.socket}")
 
-    def get_response_by_command(self, command: str, buffer_size: int = 1024):
+    def get_response_by_command(self, command: str, buffer_size: int = 1024) -> str:
         """Get response by writing a command"""
         # Send the command as request
         try:
             self.socket.sendall(command.encode('utf-8'))
-        except TimeoutError as e:
-            logger.error(e)
-            return ''
-        except UnicodeError as e:
+        except (TimeoutError, UnicodeError) as e:
             logger.error(e)
             return ''
         # Receiving data
         try:
             return self.socket.recv(buffer_size).decode('utf-8')
-        except UnicodeError as e:
+        except (TimeoutError, UnicodeError) as e:
             logger.error(e)
             return ''
 
 
 class EthernetIoModule(EthernetIoBase, ABC):
     """Base class for I/O module"""
-    pass
+    def __init__(self, io_unit: EthernetIoUnit):
+        self.io_unit = io_unit
+        self._type_code_settings = None  # Dict for type code settings
+
+    def read_configuration_status(self, address_id: int) -> dict[str, str] | None:
+        """$AA2: Read module configuration"""
+        cmd = f"${self._to_hex(address_id)}2\r"
+        rsp = self.io_unit.get_response_by_command(cmd)
+        # Get decoded response
+        dec_rsp = self.decode_response(
+            response=rsp,
+            parse={'address_id': (1, 2), 'type_code': (3, 4), 'baud_rate_code': (5, 6), 'format_code': (7, 8)}
+        )
+        # Process decoded response
+        if dec_rsp is not None:
+            dec_rsp['type'] = self._type_code_settings.get(dec_rsp.pop('type_code'))
+            return dec_rsp
+        else:
+            return None
+
+    def read_analog_input_all_channels(self, address_id: int) -> dict[str, str | float | int]:
+        """#AA: Read analog/counter inputs of all channels"""
+        cmd = f"#{self._to_hex(address_id)}\r"
+        rsp = self.io_unit.get_response_by_command(cmd)
+        return self.decode_response(
+            response=rsp,
+            parse={'data': (1, -2)},
+        )
+
+    @staticmethod
+    def _split_data_string_to_values(data_string: str, none_value: str = None) -> dict[str, float | None]:
+        """Split data string to dict of channel numbers and values by using regular expression"""
+        # Split the string with lookahead assertion, drop the first empty element
+        str_values = re.split(pattern='(?=[+-])', string=data_string)[1:]
+        if none_value is None:
+            # Convert all values to float
+            values = [float(v) for v in str_values]
+        else:
+            # Convert all values to float except none_value
+            values = [float(v) if v != none_value else None for v in str_values]
+        # Return a dict with channel keys
+        return {f'Ch{ch}': v for ch, v in enumerate(values)}
--- ebcmeasurements/Icpdas/Series_I87K.py
+++ ebcmeasurements/Icpdas/Series_I87K.py
@@ -10,7 +10,7 @@ class IoUnit(Base.EthernetIoUnit):
     def __init__(self, host: str, port: int, time_out: float = 0.5):
         super().__init__(host, port, time_out)
 
-    def read_firmware_version(self, address_id: int):
+    def read_firmware_version(self, address_id: int) -> dict[str, int | str]:
         """$AAF: Read firmware version, valid for CPU and I/O module"""
         cmd = f"${self._to_hex(address_id)}F\r"
         rsp = self.get_response_by_command(cmd)
@@ -19,7 +19,7 @@ class IoUnit(Base.EthernetIoUnit):
             parse={'address_id': (1, 2), 'firmware_version': (3, -2)}
         )
 
-    def read_module_name(self, address_id: int):
+    def read_module_name(self, address_id: int) -> dict[str, int | str]:
         """$AAM: Read module name, valid for CPU and I/O module"""
         cmd = f"${self._to_hex(address_id)}M\r"
         rsp = self.get_response_by_command(cmd)
@@ -29,11 +29,65 @@ class IoUnit(Base.EthernetIoUnit):
         )
 
 
-class Module_87019R(Base.EthernetIoModule):
-    pass
+class IoModule87013W(Base.EthernetIoModule):
+    """4-Channel RTD Analog Input Module"""
+    def __init__(self, io_unit: IoUnit):
+        super().__init__(io_unit)
+        self._type_code_settings = {
+            '20': 'Platinum 100, a = 0.00385, -100 to 100 degC (default)',
+            '21': 'Platinum 100, a = 0.00385, 0 to 100 degC',
+            '22': 'Platinum 100, a = 0.00385, 0 to 200 degC',
+            '23': 'Platinum 100, a = 0.00385, 0 to 600 degC',
+            '24': 'Platinum 100, a = 0.003916, -100 to 100 degC',
+            '25': 'Platinum 100, a = 0.003916, 0 to 100 degC',
+            '26': 'Platinum 100, a = 0.003916, 0 to 200 degC',
+            '27': 'Platinum 100, a = 0.003916, 0 to 600 degC',
+            '28': 'Nickel 120, -80 to 100 degC',
+            '29': 'Nickel 120, 0 to 100 degC',
+            '2A': 'Platinum 1000, a = 0.00385, -200 to 600 degC',
+            '2E': 'Pt 100, a = 0.00385, -200 to +200 degC',
+            '2F': 'Pt 100, a = 0.003916, -200 to +200 degC',
+            '80': 'Pt 100, a = 0.00385, -200 to +600 degC',
+            '81': 'Pt 100, a = 0.003916, -200 to +600 degC',
+        }
+
+    def read_analog_input_all_channels(self, address_id: int) -> dict[str, float | None]:
+        dec_rsp = super().read_analog_input_all_channels(address_id)  # Get the decoded response
+        return self._split_data_string_to_values(dec_rsp.pop('data'), none_value='-0000') if dec_rsp is not None else None
 
 
-if __name__ == '__main__':
-    test_unit = IoUnit('134.130.40.39', 9999)
-    for _id in range(1, 10):
-        print(test_unit.read_module_name(_id))
+class IoModule87019RW(Base.EthernetIoModule):
+    """8-channel Universal Analog Input Module with High Overvoltage Protection"""
+    def __init__(self, io_unit: IoUnit):
+        super().__init__(io_unit)
+        self._type_code_settings = {
+            '00': '-15mV to +15mV',
+            '01': '-50mV to +50mV',
+            '02': '-100mV to +100mV',
+            '03': '-500mV to +500mV',
+            '04': '-1V to +1V',
+            '05': '-2.5V to +2.5V',
+            '06': '-20mA to +20mA with 125 ohms resistor',
+            '08': '-10V to +10V',
+            '09': '-5V to +5V',
+            '0A': '-1V to +1V',
+            '0B': '-500mV to +500mV',
+            '0C': '-150mV to +150mV',
+            '0D': '-20mA to +20mA with 125 ohms resistor',
+            '0E': 'J Type',
+            '0F': 'K Type',
+            '10': 'T Type',
+            '11': 'E Type',
+            '12': 'R Type',
+            '13': 'S Type',
+            '14': 'B Type',
+            '15': 'N Type',
+            '16': 'C Type',
+            '17': 'L Type',
+            '18': 'M Type',
+            '19': 'L Type DIN43710',
+        }
+
+    def read_analog_input_all_channels(self, address_id: int) -> dict[str, float | None]:
+        dec_rsp = super().read_analog_input_all_channels(address_id)  # Get the decoded response
+        return self._split_data_string_to_values(dec_rsp.pop('data')) if dec_rsp is not None else None
